6dda3356b5293bddf6316183b9182aed6b865478
HeisenberXx <mrwhite8q@gmail.com>
2023-12-21 14:58:43 -0500

    Se añaden los cambios de Esteban

--- 10S_Page/contactenos.js
+++ 10S_Page/contactenos.js
@@ -1,11 +1,39 @@
 
+// Se crea la modal para cuando se envie el correo
+var modal = new tingle.modal({
+  footer: true,
+  stickyFooter: false,
+  closeMethods: ['overlay', 'button', 'escape'],
+  closeLabel: "Close",
+  cssClass: ['custom-class-1', 'custom-class-2'],
+  onOpen: function() {
+      console.log('modal open');
+  },
+  onClose: function() {
+      console.log('modal closed');
+  },
+  beforeClose: function() {
+      // here's goes some logic
+      // e.g. save content before closing the modal
+      return true; // close the modal
+      return false; // nothing happens
+  }
+});
+modal.setContent('<h3 style="display: flex; justify-content: center; align-items-center; font-weight: 400">Se ha enviado el correo correctamente</h3>');
+
+modal.addFooterBtn('Entendido', 'tingle-btn tingle-btn--primary', function() {
+  // here goes some logic
+  modal.close();
+});
+
+// Evento que se dispara cuando se envia el formulario
 document.addEventListener('submit', (ev) => {
   //Obteniendo los argumentos del formulario
   const nombre = document.getElementById('nombre')
   const correoElectronico = document.getElementById('correo')
   const asunto = document.getElementById('asunto')
   const mensaje = document.getElementById('mensaje')
-  console.log("probando")
+ 
   ev.preventDefault()
   const hojaDeVida = document.querySelector('#file').files[0];
   if (hojaDeVida == undefined) {
@@ -19,7 +47,7 @@ document.addEventListener('submit', (ev) => {
     <strong>Correo: </strong> ${correoElectronico.value} <br>
     <strong>Mensaje: </strong> ${mensaje.value}`,
     }).then((v) => {
-      alert('Se envio el correo');
+      modal.open();
     });
   } else {
     const lector = new FileReader()
@@ -45,7 +73,7 @@ document.addEventListener('submit', (ev) => {
           }
         ]
       }).then((v) => {
-        alert('Se envio el correo');
+        modal.open();
       });
     }
   }
